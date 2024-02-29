13ee85e2d4cae03eff180170952b9c084140a7ec
unknown <victorignacio1906@gmail.com>
2024-02-29 19:55:59 -0300

    finalizou a tela

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Login from "../components/login"
+import { Victor_Mono } from 'next/font/google'
 export default function Home() {
   return (
     <main className={styles.main}>
-     <div className={styles.conteiner}><Login></Login></div>
+      <div className={styles.conteiner}><Login></Login></div>
     </main>
-    
   )
+  
+
 }
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -15,17 +15,28 @@ const Login = () => {
         <input className={style.username1} placeholder='Digite sua senha...'></input>
       </div>
       <button className={style.botao}>logar</button>
-      <a className={style.novaconta} onClick={()=>setnewpassword(false)}>criar nova conta</a>
+      <a className={style.novaconta} onClick={() => setnewpassword(false)}>criar nova conta</a>
     </div>;
-  return <div className={style.conteiner}><h1 className={style.titulo}>Criar nova conta</h1>
-  <div className={style.caixaiput2}><Icon icon="ic:round-email" width='30px' />
-  <input className={style.username}></input></div>
-  
-  
-  
-  
-  </div>
-  
+ return <div className={style.conteiner}>
+ <h1 className={style.titulo}> Criar uma conta</h1>
+ <div className={style.caixaiput}><Icon icon="ic:round-email" width='30px' />
+   <input className={style.username} placeholder='Digite seu nome de usuario...'></input>
+ </div>
+ <div className={style.caixaiput}><Icon icon="ic:round-email" width='30px' />
+   <input className={style.username} placeholder='Digite seu e-mail...'></input>
+ </div>
+ <div className={style.caixaiput}><Icon icon="ic:round-email" width='30px' />
+   <input className={style.username} placeholder='Digite seu e-mail novamente...'></input>
+ </div>
+ <div className={style.caixaiput}><Icon icon="mdi:password" width='30px' />
+   <input className={style.username1} placeholder='Digite sua senha...'></input>
+ </div>
+ <div className={style.caixaiput}><Icon icon="mdi:password" width='30px' />
+   <input className={style.username1} placeholder='Digite sua senha...'></input>
+ </div>
+ <button className={style.botao}>Criar conta</button>
+</div>;
+
 }
 
 export default Login;
